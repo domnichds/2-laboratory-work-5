ba73c2c86d00d4183c349ee874c7121ad294cc80
Danil Domnich <domni4.d2@yandex.ru>
2025-03-28 22:47:52 +0300

    Добавить перемещение данных между контейнерами и работу с файлами

--- 2-laboratory-work-5.cpp
+++ 2-laboratory-work-5.cpp
@@ -5,6 +5,8 @@
 #include <deque>
 #include <fstream>
 #include <sstream>
+#include <set>
+#include <unordered_set>
 
 class Student
 {
@@ -28,14 +30,18 @@ public:
 		this->grade_book_number = grade_book_number;
 		for (short i = 0; i < 4; i++) this->marks[i] = marks[i];
 	}
-	Student(std::string full_name, unsigned int group, unsigned int grade_book_number, int marks[4])
-	{
-		this->full_name = full_name;
-		this->group = group;
-		this->grade_book_number = grade_book_number;
-		for (short i = 0; i < 4; i++) this->marks[i] = marks[i];
+	Student& operator=(const Student& other) {
+		if (this != &other) {
+			full_name = other.full_name;
+			group = other.group;
+			grade_book_number = other.grade_book_number;
+			for (int i = 0; i < 4; i++) {
+				marks[i] = other.marks[i];
+			}
+		}
+		return *this;
 	}
-	bool operator<(const Student& other)
+	bool operator<(const Student& other) const
 	{
 		int avg_first = 0;
 		int avg_second = 0;
@@ -85,4 +91,62 @@ namespace std
 				hash<int>{}(student.marks[3]);
 		}
 	};
+}
+
+std::vector<Student> readData(std::string path)
+{
+	std::vector<Student> students;
+	std::ifstream file(path);
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::stringstream ss(line);
+		std::string name, group, grade_book_number, marks_string;
+		int marks[4] = { 0, 0, 0, 0 };
+		std::getline(ss, name, ';');
+		std::getline(ss, group, ';');
+		std::getline(ss, grade_book_number, ';');
+		std::getline(ss, marks_string, ';');
+
+		std::stringstream marks_ss(marks_string);
+		std::string mark;
+		short index = 0;
+		while (std::getline(marks_ss, mark, ',') && index < 4)
+		{
+			marks[index++] = std::stoi(mark);
+		}
+		students.emplace_back(name, std::stoi(group), std::stoi(grade_book_number), marks);
+	}
+	return students;
+}
+
+template <class T>
+void writeData(const T& data, const std::string& path)
+{
+	std::ofstream file(path, std::ios::app);
+	if (!file) return;
+	for (const auto& student : data) file << student;
+	file << "=======================" << std::endl;
+	file.close();
+
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "ru_RU.1251");
+	std::string input_path = argv[1];
+	std::string output_path = argv[2];
+	std::vector<Student> students_vector = readData(input_path);
+
+	std::set<Student> studentSet;
+	std::copy(students_vector.begin(), students_vector.end(),
+		std::inserter(studentSet, studentSet.end()));
+
+	std::unordered_set<Student> studentUnorderedSet;
+	std::copy(students_vector.begin(), students_vector.end(),
+		std::inserter(studentUnorderedSet, studentUnorderedSet.end()));
+
+	std::ofstream file(output_path, std::ios::trunc);
+	writeData(studentSet, output_path);
+	writeData(studentUnorderedSet, output_path);
 }
